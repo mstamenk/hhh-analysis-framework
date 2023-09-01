8447774c6266ee9e50cedd35a872926403431a6a
Marko Stamenkovic <marko.stamenkovic@cern.ch>
2023-09-01 13:14:19 -0400

    Removing files

--- spanet-inference/condor-run/jobs/job_SingleMuon_2017_38.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cmsrel CMSSW_12_5_2
-cd CMSSW_12_5_2/src
-cmsenv
-ulimit -s unlimited
-export MYROOT=$(pwd)
-export PYTHONPATH=$PYTHONPATH:$MYROOT 
-python3 /isilon/data/users/mstamenk/hhh-6b-producer/master/CMSSW_12_5_2/src/hhh-master/hhh-analysis-framework/spanet-inference//predict_spanet.py --f_in SingleMuon --year 2017 --batch_size 100000 --batch_number 38
