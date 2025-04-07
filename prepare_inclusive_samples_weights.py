70fdb6a92d848c531f3a96b0234aa59db0c24a40
Marko Stamenkovic <mstamenk@pbrux40cit.hep.brown.edu>
2025-04-07 11:47:31 -0400

    Updating master

--- prepare_inclusive_samples_weights.py
+++ prepare_inclusive_samples_weights.py
@@ -5,7 +5,7 @@ import os, ROOT
 import glob
 
 from machinelearning import init_bdt_boosted, add_bdt_boosted, init_bdt, add_bdt
-from utils import init_mhhh, addMHHH, initialise_df,triggersCorrections, save_variables, matching_variables, hlt_paths
+from utils import init_mhhh, addMHHH, initialise_df,triggersCorrections, save_variables, matching_variables, hlt_paths, applySelection
 from calibrations import btag_init, addBTagSF, addBTagEffSF
 
 from hhh_variables import add_hhh_variables, add_hhh_variables_resolved
@@ -15,7 +15,7 @@ ROOT.ROOT.EnableImplicitMT()
 
 import argparse
 parser = argparse.ArgumentParser(description='Args')
-parser.add_argument('-v','--version', default='v28')
+parser.add_argument('-v','--version', default='v33')
 parser.add_argument('--year', default='2018')
 parser.add_argument('--f_in', default = 'GluGluToHHHTo6B_SM')
 args = parser.parse_args()
@@ -31,7 +31,7 @@ path = '/isilon/data/users/mstamenk/hhh-6b-producer/CMSSW_11_1_0_pre5_PY3/src/Ph
 
 print(path)
 
-output = '/isilon/data/users/mstamenk/eos-triple-h/%s-parts-no-lhe/mva-inputs-%s/'%(version,year)
+output = '/isilon/data/users/mstamenk/eos-triple-h/%s-parts-no-hlt/mva-inputs-%s/'%(version,year)
 
 
 inclusive_resolved = 'inclusive_resolved-weights'
@@ -40,17 +40,9 @@ inclusive_boosted = 'inclusive-weights'
 
 cut_resolved = 'nsmalljets >= 4 && nprobejets == 0'
 #cut_boosted = 'nprobejets > 0 '
-cut_boosted = '(nprobejets > 0) || (nsmalljets >= 4 && nprobejets == 0 && nloosebtags >= 4)'
+cut_boosted = '(nprobejets > 0) || (nsmalljets >= 4 && nprobejets == 0)'
 
 #cut_boosted = '(nprobejets > -1)'
-
-if not os.path.isdir(output + '/' + inclusive_resolved):
-    print("Creating %s"%(output + '/' + inclusive_resolved))
-    os.makedirs(output + '/' + inclusive_resolved)
-
-if not os.path.isdir(output + '/' + inclusive_boosted):
-    print("Creating %s"%(output + '/' + inclusive_boosted))
-    os.makedirs(output + '/' + inclusive_boosted)
 
 files = glob.glob(path + '/' + '*.root')
 
@@ -68,17 +60,34 @@ if '2016APV' in year:
     btag_init('2016preVFP')
 elif '2016' in year:
     btag_init('2016postVFP')
-else:
+elif '2017' in year or '2018' in year:
     btag_init(year)
 
 if '2017' in year:
     data_files = ['BTagCSV.root']
-else:
+elif '2016' in year or '2018' in year:
     data_files = ['JetHT.root']
+else:
+    data_files = ['JetMET.root']
 
 #for f_in in files:
 
 f_in = args.f_in
+
+if 'jes_' in f_in or 'jer_' in f_in or 'jmr_' in f_in:
+    splits = f_in.split('/')
+    syst = splits[len(splits) - 3]
+    inclusive_boosted += '/' + syst
+
+#if not os.path.isdir(output + '/' + inclusive_resolved):
+#    print("Creating %s"%(output + '/' + inclusive_resolved))
+#    os.makedirs(output + '/' + inclusive_resolved)
+
+if not os.path.isdir(output + '/' + inclusive_boosted):
+    print("Creating %s"%(output + '/' + inclusive_boosted))
+    os.makedirs(output + '/' + inclusive_boosted)
+
+
 f_name = os.path.basename(f_in)
 print(f_name)
 
@@ -124,9 +133,6 @@ if 'HLT_PFMET100_PFMHT100_IDTight_CaloBTagDeepCSV_3p1' not in list_inputs:
 if 'HLT_AK8PFJet330_PFAK8BTagCSV_p17' not in list_inputs:
     df = df.Define('HLT_AK8PFJet330_PFAK8BTagCSV_p17','get_false()')
 
-if 'HLT_PFHT300PT30_QuadPFJet_75_60_45_40_TriplePFBTagCSV_3p0' not in list_inputs:
-    df = df.Define('HLT_PFHT300PT30_QuadPFJet_75_60_45_40_TriplePFBTagCSV_3p0','get_false()')
-
 
 if 'HLT_AK8PFHT750_TrimMass50' not in list_inputs:
     df = df.Define('HLT_AK8PFHT750_TrimMass50','get_false()')
@@ -149,34 +155,50 @@ if 'HLT_PFHT430_SixPFJet40_PFBTagCSV_1p5' not in list_inputs:
 if 'HLT_AK8PFJet450' not in list_inputs:
     df = df.Define('HLT_AK8PFJet450','get_false()')
 
-hlt = hlt_paths[year]
-df = df.Filter(hlt)
 
-if first:
-    init_bdt(df,year)
-    init_bdt_boosted(df,year)
-    first = False
+
+if 'HLT_HT300PT30_QuadJet_75_60_45_40_TripeCSV_p07' not in list_inputs:
+    df = df.Define('HLT_HT300PT30_QuadJet_75_60_45_40_TripeCSV_p07','get_false()')
+
+if 'HLT_PFHT300PT30_QuadPFJet_75_60_45_40_TriplePFBTagCSV_3p0' not in list_inputs:
+    df = df.Define('HLT_PFHT300PT30_QuadPFJet_75_60_45_40_TriplePFBTagCSV_3p0','get_false()')
+
+
+
+#if first:
+#    init_bdt(df,year)
+#    init_bdt_boosted(df,year)
+#    first = False
 
 df = initialise_df(df,year,f_in)
 
-df,masses,pts,etas,phis,drs = add_hhh_variables_resolved(df)
+if '2022' in year or '2023' in year:
+    df,masses,pts,etas,phis,drs = add_hhh_variables_resolved(df, False)
+else:
+    df,masses,pts,etas,phis,drs = add_hhh_variables_resolved(df)
 
-df = add_bdt(df,year)
-df = add_bdt_boosted(df,year)
-if 'JetHT' not in f_in and 'BTagCSV' not in f_in and 'SingleMuon' not in f_in:
+#df = add_bdt(df,year)
+#df = add_bdt_boosted(df,year)
+if 'JetHT' not in f_in and 'BTagCSV' not in f_in and 'SingleMuon' not in f_in and 'JetMET' not in f_in:
     df = matching_variables(df)
 
 #df = df.Define('ProbMultiH','ProbHHH + ProbHH4b + ProbHHH4b2tau + ProbHH2b2tau')
 
-df_resolved = df.Filter(cut_resolved)
-df_boosted = df.Filter(cut_boosted)
+#df_resolved = df.Filter(cut_resolved)
+#df_boosted = df.Filter(cut_boosted)
+
+df_boosted = applySelection(df,year)
 
 print("Running on %s"%f_in)
 print("Doing resolved")
 
 #to_save = [str(el) for el in df_boosted.GetColumnNames() if 'L1_' not in str(el) and 'v_' not in str(el) and 'MassRegressed' not in str(el) and 'bcand' not in str(el) and 'boostedTau_' not in str(el) and 'PNet' not in str(el)]
-to_save = [str(el) for el in df_boosted.GetColumnNames() if 'L1_' not in str(el) and 'v_' not in str(el) and 'MassRegressed' not in str(el) and 'bcand' not in str(el) and 'boostedTau_' not in str(el) and 'LHE' not in str(el)]
-print(to_save)
+to_save = [str(el) for el in df_boosted.GetColumnNames() if 'L1_' not in str(el) and 'v_' not in str(el) and 'MassRegressed' not in str(el) and 'bcand' not in str(el) and 'boostedTau_' not in str(el) ]
+
+if 'QCD' in args.f_in:
+    to_save = [el for el in to_save if 'LHE' not in el]
+
+#print(to_save)
 print(len(to_save))
 #if not os.path.isfile( output + '/' + inclusive_resolved + '/' + f_name):
 #df_resolved.Snapshot('Events', output + '/' + inclusive_resolved + '/' + f_name, to_save)
@@ -185,8 +207,10 @@ print("Doing boosted")
 #if not os.path.isfile( output + '/' + inclusive_boosted + '/' + f_name):
 #print(save_variables + ['eventWeight']+masses+pts+etas+phis+drs)
 
-df_boosted.Snapshot('Events', output + '/' + inclusive_boosted + '/' + f_name, to_save)
-
+if df_boosted.Count().GetValue() > 0:
+    df_boosted.Snapshot('Events', output + '/' + inclusive_boosted + '/' + f_name, to_save)
+else:
+    print("0 events pass the selection")
 
 print("All done!")
 
